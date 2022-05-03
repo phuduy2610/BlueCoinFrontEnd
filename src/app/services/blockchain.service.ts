14b67294c0e6f99c917533c7bfa9acc2f5779336
phuduy2610 <phuduy2610@gmail.com>
2022-05-03 16:38:26 +0700

    - Fix import error - Add component to view blockchain

--- src/app/services/blockchain.service.ts
+++ src/app/services/blockchain.service.ts
@@ -1,9 +1,33 @@
-import { Injectable } from '@angular/core';
-
+//@ts-ignore
+import {BlockChain} from 'CMNBlockChain/src/blockchain';
+import {Injectable } from '@angular/core';
+import {ec} from 'elliptic';
 @Injectable({
   providedIn: 'root'
 })
 export class BlockchainService {
+  public blockchainInstance = new BlockChain();
+  public walletKeys:any[] = [];
+  constructor() {
+    this.blockchainInstance.difficulty = 1;
+    this.blockchainInstance.minePendingTransactions('my-wallet-address');
 
-  constructor() { }
+    this.generateWalletKeys();
+   }
+
+  getBlocks(){
+    return this.blockchainInstance.chain;
+  }
+
+  private generateWalletKeys(){
+    const eC = new ec('secp256k1');
+    const key = eC.genKeyPair();
+
+    this.walletKeys.push({
+      keyObj:key,
+      publicKey:key.getPublic('hex'),
+      privateKey:key.getPrivate('hex'),
+    });
+
+  }
 }
