7ab0119bac97323b484aaf262bdef3f987ce9b3d
phuduy2610 <phuduy2610@gmail.com>
2022-05-03 21:14:31 +0700

    - Add routing route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-viewer.component';
 import { BlockViewComponent } from './components/block-view/block-view.component';
 import { TransactionsTableComponent } from './components/transactions-table/transactions-table.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { CreateTransactionComponent } from './pages/create-transaction/create-transaction.component';
+import { PendingTransactionComponent } from './pages/pending-transaction/pending-transaction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlockchainViewerComponent,
     BlockViewComponent,
-    TransactionsTableComponent
+    TransactionsTableComponent,
+    SettingsComponent,
+    CreateTransactionComponent,
+    PendingTransactionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
